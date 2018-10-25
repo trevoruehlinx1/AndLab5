a7107b4fe92051a4158954012c32fa7d49657097
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2018-10-25 10:07:41 -0700

    Solution Files From Lab 4

---
+++ PigGame/MainActivity.cs
@@ -0,0 +1,166 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PigGame
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        PigGameLogic gameLogic;
+        string player1Name;
+        string player2Name;
+        int player1Score;
+        int player2Score;
+        int pointsForTurn;
+        int lastRollValue;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // create all of the views
+            var newGameButton = FindViewById<Button>(Resource.Id.newGameButton);
+            var rollButton = FindViewById<Button>(Resource.Id.rollButton);
+            var newPlayersButton = FindViewById<Button>(Resource.Id.newPlayersPuttonButton);
+            var endTurnButton = FindViewById<Button>(Resource.Id.endTurnButton);
+            var nameEditText1 = FindViewById<EditText>(Resource.Id.textInputEditText1);
+            var nameEditText2 = FindViewById<EditText>(Resource.Id.textInputEditText2);
+            var score1Label = FindViewById<TextView>(Resource.Id.scoreLabel1);
+            var score2Label = FindViewById<TextView>(Resource.Id.scoreLabel2);
+            var scoreTextView1 = FindViewById<TextView>(Resource.Id.scoreTextView1);
+            var scoreTextView2 = FindViewById<TextView>(Resource.Id.scoreTextView2);
+            var whosTurnLabel = FindViewById<TextView>(Resource.Id.whosTurnLabel);
+            var pointsForTurnTextView = FindViewById<TextView>(Resource.Id.pointsForTurnTextView);
+            var diceImageView = FindViewById<ImageView>(Resource.Id.diceImageView);
+
+            //if the game has been saved get the page out of the json
+            if (savedInstanceState != null)
+            {
+                gameLogic = new PigGameLogic();
+                string jsonGame = savedInstanceState.GetString("pageInJson");
+                gameLogic = JsonConvert.DeserializeObject<PigGameLogic>(jsonGame);
+
+                score1Label.Text = gameLogic.Player1Name + "'s Score";
+                score2Label.Text = gameLogic.Player2Name + "'s Score";
+                scoreTextView1.Text = gameLogic.Player1Score.ToString();
+                scoreTextView2.Text = gameLogic.Player2Score.ToString();
+                pointsForTurnTextView.Text = gameLogic.PointsForTurn.ToString();
+                diceImageView.SetImageResource(gameLogic.SetDiceImage());
+
+                if (gameLogic.Player1Turn == true)
+                    whosTurnLabel.Text = gameLogic.Player1Name + "'s Turn";
+                else
+                    whosTurnLabel.Text = gameLogic.Player2Name + "'s Turn";
+
+                nameEditText1.Enabled = false;
+                nameEditText2.Enabled = false;
+
+            }
+            else
+            {
+                //Create a new game logic
+                gameLogic = new PigGameLogic();
+                player1Name = "";
+                player2Name = "";
+                player1Score = 0;
+                player2Score = 0;
+                rollButton.Enabled = false;
+                endTurnButton.Enabled = false;
+                diceImageView.SetImageResource(Resource.Drawable.pig);
+            }
+            newGameButton.Click += (sender, e) =>
+            {
+                diceImageView.SetImageResource(Resource.Drawable.pig);
+                gameLogic = new PigGameLogic();
+                if (nameEditText1.Enabled == true && nameEditText1.Text == ""
+                    || nameEditText2.Text == "" && nameEditText2.Enabled == true)
+                {
+                    whosTurnLabel.Text = "Enter a name for each of the players";
+                }
+
+                else
+                {
+                    rollButton.Enabled = true;
+                    endTurnButton.Enabled = true;
+                    player1Name = nameEditText1.Text;
+                    player2Name = nameEditText2.Text;
+                    gameLogic.GetPlayerNames(player1Name, player2Name);
+                    score1Label.Text = player1Name + "'s Score";
+                    score2Label.Text = player2Name + "'s Score";
+                    scoreTextView1.Text = gameLogic.Player1Score.ToString();
+                    scoreTextView2.Text = gameLogic.Player2Score.ToString();
+                    whosTurnLabel.Text = player1Name + "'s Turn";
+                    pointsForTurnTextView.Text = "0";
+
+                    nameEditText1.Enabled = false;
+                    nameEditText2.Enabled = false;
+                }
+            };
+            rollButton.Click += (sender, e) =>
+            {
+                string roll = gameLogic.RollDie();
+                if (roll != "0")
+                    pointsForTurnTextView.Text = roll;
+                else
+                {
+                    rollButton.Enabled = false;
+                    whosTurnLabel.Text = "You rolled a 1";
+                    pointsForTurnTextView.Text = roll;
+                }
+                diceImageView.SetImageResource(gameLogic.SetDiceImage());
+            };
+            endTurnButton.Click += (sender, e) =>
+            {
+                gameLogic.EndTurn();
+                rollButton.Enabled = true;
+                if (gameLogic.CheckForWinner() != "")
+                {
+                    whosTurnLabel.Text = gameLogic.CheckForWinner();
+                    rollButton.Enabled = false;
+                    endTurnButton.Enabled = false;
+                }
+                else
+                {
+                    scoreTextView1.Text = gameLogic.Player1Score.ToString();
+                    scoreTextView2.Text = gameLogic.Player2Score.ToString();
+                    pointsForTurnTextView.Text = gameLogic.PointsForTurn.ToString();
+                    if (gameLogic.Player1Turn == false)
+                        whosTurnLabel.Text = gameLogic.Player2Name + "'s Turn";
+                    else
+                        whosTurnLabel.Text = gameLogic.Player1Name + "'s Turn";
+                }
+            };
+            newPlayersButton.Click += (sender, e) =>
+            {
+                nameEditText1.Enabled = true;
+                nameEditText2.Enabled = true;
+                nameEditText1.Text = "";
+                nameEditText2.Text = "";
+                score1Label.Text = "";
+                score2Label.Text = "";
+                scoreTextView1.Text = "";
+                scoreTextView2.Text = "";
+                rollButton.Enabled = false;
+                endTurnButton.Enabled = false;
+                whosTurnLabel.Text = "Enter a name for each of the players";
+            };
+        }
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+
+            string pageInJson = JsonConvert.SerializeObject(this.gameLogic, Formatting.Indented);
+            outState.PutString("pageInJson",pageInJson);
+
+            base.OnSaveInstanceState(outState);
+        }
+    }
+}
+
---
+++ PigGame/PigGameLogic.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PigGame
+{
+    class PigGameLogic
+    {
+        public PigGameLogic()
+        {
+
+        }
+        public PigGameLogic(string name1, string name2, int score1, int score2, Boolean turn, int turnPoints, int lastRoll)
+        {
+            Player1Name = name1;
+            Player2Name = name2;
+            Player1Score = score1;
+            Player2Score = score2;
+            Player1Turn = turn;
+            PointsForTurn = turnPoints;
+            RollValue = lastRoll;
+        }
+        public Boolean Player1Turn = true;
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+        public int PointsForTurn { get; set; }
+        public int RollValue { get; set; }
+        
+
+
+        public string RollDie()
+        {
+            Random random = new Random();
+            RollValue = random.Next(1, 6);
+            if (RollValue == 1)
+                PointsForTurn = 0;
+            else
+                PointsForTurn += RollValue;
+            return PointsForTurn.ToString();
+        }
+
+        public void EndTurn()
+        {
+            if (Player1Turn == true)
+            {
+                if (Player1Score + PointsForTurn <= 100)
+                {
+                    Player1Score += PointsForTurn;
+                    Player1Turn = false;
+                }
+                else
+                    Player1Turn = false;
+                PointsForTurn = 0;
+            }
+            else
+            {
+                if(Player2Score + PointsForTurn <= 100)
+                {
+                    Player2Score += PointsForTurn;
+                    Player1Turn = true;
+                }
+                else
+                    Player1Turn = true;
+                PointsForTurn = 0;
+            }
+            CheckForWinner();
+        }
+        public void GetPlayerNames(string name1, string name2)
+        {
+            Player1Name = name1;
+            Player2Name = name2;
+        }
+        public string CheckForWinner()
+        {
+            if(Player1Turn == true)
+            {
+                if (Player1Score == 100)
+                    return Player2Name + " wins the game";
+                if (Player2Score == 100)
+                    return Player2Name + " wins the game!";
+                else
+                    return "";
+            }
+            else
+                return "";
+        }
+        public int SetDiceImage()
+        {
+            if (RollValue == 0)
+                return Resource.Drawable.pig;
+            int[] diceImages = new int[6]
+            {
+                Resource.Drawable.Die1,
+                Resource.Drawable.Die2,
+                Resource.Drawable.Die3,
+                Resource.Drawable.Die4,
+                Resource.Drawable.Die5,
+                Resource.Drawable.Die6
+            };
+            return diceImages[RollValue - 1];
+        }
+    }
+}
